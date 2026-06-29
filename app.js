526b7e2e4c047933c907866d200fb27bced93b3e
lucianamenotti <lucianamenottiipf@gmail.com>
2026-06-29 19:17:40 -0300

    cree rutas del crud e integrarlas al servidor principal

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 import express from "express";
 import sequelize from "./src/config/database.js";
+import movieRoutes from "./src/routes/movie.routes.js";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api/movies", movieRoutes);
+
 const PORT = 3000;
 
 const startServer = async () => {
   try {
-    await sequelize.authenticate();
-    console.log("Conexion a la base de datos exitosa.");
-    
+    await sequelize.sync();
+    console.log("Base de datos sincronizada");
+
     app.listen(PORT, () => {
       console.log("Servidor corriendo en http://localhost:" + PORT);
     });
   } catch (error) {
-    console.log("Error al conectar a la base de datos:", error);
+    console.log("Error al iniciar el servidor o conectar la base de datos:", error);
   }
 };
 
---
+++ src/routes/movie.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { getMovies, getMovieById, createMovie, updateMovie, deleteMovie } from "../controllers/movie.controllers.js";
+
+const router = Router();
+
+router.get("/", getMovies);
+router.get("/:id", getMovieById);
+router.post("/", createMovie);
+router.put("/:id", updateMovie);
+router.delete("/:id", deleteMovie);
+
+export default router;
